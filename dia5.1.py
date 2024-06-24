6c7d2194726399365d99d13855c7c64f37abdcb8
titogarcete <144870935+titogarcete@users.noreply.github.com>
2024-06-24 19:11:39 -0400

    Add files via upload

---
+++ dia5.1.py
@@ -0,0 +1,10 @@
+# Definir las listas de claves y valores
+claves = ['a', 'b', 'c', 'd']
+valores = [1, 2, 3, 4]
+
+# Crear el diccionario a partir de las listas
+diccionario = dict(zip(claves, valores))
+
+# Mostrar el diccionario resultante
+print("El diccionario creado es:")
+print(diccionario)
